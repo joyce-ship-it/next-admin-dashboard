75478d63fff624cca5eeedf7e56b069d9108938b
joycethomas <joycethomas2244@outlook.com>
2025-11-27 11:50:42 +0530

    add Dashboard cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 export default function Home() {
   return (
     <>
       <div className="mb-5 flex flex-col justify-between gap-5 md:flex-row">
-        <DashboardCard />
+        <DashboardCard
+          title="Posts"
+          icon={<Newspaper size={72} className="text-slate-500" />}
+          count={100}
+        />
+        <DashboardCard
+          title="Categories"
+          icon={<Folder size={72} className="text-slate-500" />}
+          count={4}
+        />
+        <DashboardCard
+          title="Users"
+          icon={<User size={72} className="text-slate-500" />}
+          count={1245}
+        />
+        <DashboardCard
+          title="Comments"
+          icon={<MessageCircle size={72} className="text-slate-500" />}
+          count={1200}
+        />
       </div>
     </>
   );
--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { Card, CardContent } from "../ui/card";
-import { Newspaper } from "lucide-react";
-export default function DashboardCard() {
+import { LucideIcon, Newspaper } from "lucide-react";
+
+interface DashboardCardProps {
+  title: string;
+  count: number;
+  icon: React.ReactElement<LucideIcon>;
+}
+export default function DashboardCard({
+  title,
+  count,
+  icon,
+}: DashboardCardProps) {
   return (
     <Card className="bg-slate-100 p-4 pb-0 dark:bg-slate-800">
       <CardContent>
         <h3 className="pb-4 text-center text-3xl font-bold text-slate-500 dark:text-slate-200">
-          Posts
+          {title}
         </h3>
         <div className="flex items-center justify-center gap-5">
-          <Newspaper size={72} className="text-slate-500" />
+          {icon}
           <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">
-            45
+            {count}
           </h3>
         </div>
       </CardContent>
