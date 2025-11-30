63d2c3af0899ce3559577127fb7ab64c57978c3e
joycethomas <joycethomas2244@outlook.com>
2025-11-30 21:14:11 +0530

    add pagination component

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PostsTable from "@/components/posts/PostsTable";
 import BackButton from "@/components/BackButton";
+import PostsPagination from "@/components/posts/PostsPagination";
 export default function PostsPage() {
   return (
     <>
       <BackButton link="/" text="Go Back"></BackButton>
       <PostsTable />
+      <PostsPagination />
     </>
   );
 }
--- components/posts/PostsPagination.tsx
+++ components/posts/PostsPagination.tsx
@@ -1,5 +1,39 @@
 import React from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 export default function PostsPagination() {
-  return <div>PostsPagination</div>;
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#" isActive>
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationNext href="#" />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
 }
